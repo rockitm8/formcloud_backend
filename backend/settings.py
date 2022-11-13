8ebb8690dd6bc53df055acc8e868ba6e147d6644
Abdullah Mehboob <abdullah2013120@gmail.com>
2022-11-14 02:33:00 +0500

    commit

--- backend/settings.py
+++ backend/settings.py
@@ -148,7 +148,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 import os
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
